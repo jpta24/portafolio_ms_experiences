d2445b243c372e3697985897d48b7b04f7e13552
Jpta24 <jeanpierretorres@gmail.com>
2023-11-27 14:06:48 +0100

    setup done

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,12 +1,13 @@
 const router = require("express").Router();
 
 router.get("/", (req, res, next) => {
-  res.json("All good in here");
+  res.json("All good in here in MS_Experiences");
 });
 
 router.get("/test", (req, res, next) => {
-  res.json("Test from Backend WORKING");
+  res.json("Test from MS_Experiences");
 });
+
 
 
 module.exports = router;
--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const app = require('./app');
 
 const {connectDBs} = require('./db/index');
 
-// ℹ️ Sets the PORT for our app to have access to it. If no env has been set, we hard code it to 5008
-const PORT = process.env.PORT || 5008;
+// ℹ️ Sets the PORT for our app to have access to it. If no env has been set, we hard code it to 5013
+const PORT = process.env.PORT || 5013;
 
 connectDBs().then(() => {
 	app.listen(PORT, () => {
